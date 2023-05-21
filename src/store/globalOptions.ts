e74ebb9e37427b37024dcf8182bca845bf63a74c
jerrywu <jerrywu001@gmail.com>
2023-05-21 10:55:22 +0800

    docs: update

--- src/store/globalOptions.ts
+++ src/store/globalOptions.ts
@@ -3,6 +3,6 @@ import { reactive } from 'vue';
 
 export const globalOptions = reactive<{ [key: string]: ToastContainerOptions }>({});
 
-export const globalCache = reactive<{
+export const globalCache = reactive({} as {
   lastUrl: string;
-}>({});
+});
