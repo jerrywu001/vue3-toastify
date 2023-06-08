60bb7e9d47e5c8a2f41f68ea5f14c027a558e394
jerrywu <jerrywu001@gmail.com>
2023-06-08 14:27:39 +0800

    docs: pkg

--- __tests__/tools.ts
+++ __tests__/tools.ts
@@ -2,3 +2,7 @@ import { toast } from '../src';
 import { Default } from '../src/utils/constant';
 
 export const positionClass = (position = toast.POSITION.TOP_RIGHT) => `${Default.CSS_NAMESPACE}__toast-container--${position}`;
+
+it('Test', () => {
+  expect(1 + 1).toBe(2);
+});
