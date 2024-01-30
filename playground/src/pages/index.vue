35b22dbcadb3eab0a2a45b500ef74bdbac278d0e
jerrywu <jerrywu001@gmail.com>
2024-01-30 11:12:51 +0800

    docs: update sponsor

--- playground/src/pages/index.vue
+++ playground/src/pages/index.vue
@@ -109,6 +109,19 @@ const displayPromise = () => {
       @on-change="onOptionsChange"
     />
     <ToastCode :options="options"></ToastCode>
+
+    <br />
+    <br />
+
+    <Divider />
+
+    <h2 align="center" style="font-size: 26px; font-weight: bold; padding: 10px 0 22px 0;">Special Sponsor</h2>
+
+    <p align="center">
+      <a target="_blank" rel="external dofollow" title="banning sensor" href="https://www.bnsense.com/">
+        <img alt="special sponsor appwrite" src="https://www.bnsense.com/uploads/LOGO/imgs/logo_1704355682323.png" width="160">
+      </a>
+    </p>
   </div>
 </template>
 
